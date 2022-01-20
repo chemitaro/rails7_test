4ecf2aa3689f9b1013c40c9fcc4cb4e8123e9bd0
yuta iwasawa <iwasi_44@hotmail.com>
2022-01-21 05:54:10 +0900

    stimulusの不具合修正

--- app/javascript/controllers/reset-form_controller.js
+++ app/javascript/controllers/reset-form_controller.js
@@ -1,7 +1,9 @@
-import { Controller } from "stimulus"
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+
   reset() {
     this.element.reset()
   }
+
 }
