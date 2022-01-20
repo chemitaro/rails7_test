8c0908cc90bcd5b0f3aab00474cdfacfe931db22
yuta iwasawa <iwasi_44@hotmail.com>
2022-01-20 14:43:11 +0900

    room,messageの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "rooms#index"
-  resources :rooms
+  resources :rooms do
+    resources :messages
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
